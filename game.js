36c81751dde2d55452b0f84a19f633792a1f8cd3
summerhanyuezheng <summer199303@gmail.com>
2023-04-17 14:48:54 -0700

    created variables and functions for simon buttons

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-alert("hey!")
+const gamePattern = []
+
+function nextSequence() {
+    // generate random number from 0-3
+    var randomNumber = Math.floor(Math.random() * 4)
+    return randomNumber
+}
+
+const buttonColours = ["red", "blue", "green", "yellow"]
+
+var randomChosenColor = buttonColours[nextSequence()]
+gamePattern.push(randomChosenColor)
