1a0ac22cd64cc779cf1a6f9b97997389fdf4a3f2
summerhanyuezheng <summer199303@gmail.com>
2023-04-17 15:36:49 -0700

    added flash animation and audios; tested in chrome console

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 const gamePattern = []
+const buttonColours = ["red", "blue", "green", "yellow"]
 
 function nextSequence() {
     // generate random number from 0-3
     var randomNumber = Math.floor(Math.random() * 4)
-    return randomNumber
+    var randomChosenColor = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColor)
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3" )
+    audio.play()
 }
 
-const buttonColours = ["red", "blue", "green", "yellow"]
 
-var randomChosenColor = buttonColours[nextSequence()]
-gamePattern.push(randomChosenColor)
+
+
+
