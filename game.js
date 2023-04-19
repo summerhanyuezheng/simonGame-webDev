da65e2fee5db3ca65409c4fb08dc318a8703aa70
summerhanyuezheng <summer199303@gmail.com>
2023-04-19 01:58:37 -0700

    added checkAnswer function; the game now is functioning;

--- game.js
+++ game.js
@@ -5,6 +5,60 @@ const userClickedPattern = []
 var started = false
 var level = 0
 
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr('id')
+    userClickedPattern.push(userChosenColor)
+    playSound(userChosenColor)
+    animatePress(userChosenColor)
+    //check answer after user clicks, only need to check the last in the array
+    checkAnswer(userClickedPattern.length - 1)
+
+});
+
+
+
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel]=== userClickedPattern[currentLevel]) {
+        console.log("success")
+        //check if user has finished the current sequence
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(function() {
+                nextSequence()
+                
+            }, 1000);
+        }
+    else {
+        console.log("wrong")
+    }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+function nextSequence() {
+
+    level++
+    $("h1").text("Level " + level)
+    // generate random number from 0-3
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColor = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColor)
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor)
+    
+}
+
+
+
 $(document).keypress(function() {
     if (!started){
         started = true
@@ -30,28 +84,11 @@ function animatePress(currentColor){
 
 
 
-function nextSequence() {
-
-    level++
-    $("h1").text("Level " + level)
-    // generate random number from 0-3
-    var randomNumber = Math.floor(Math.random() * 4)
-    var randomChosenColor = buttonColours[randomNumber]
-    gamePattern.push(randomChosenColor)
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor)
-    
-}
 
 
 
-$(".btn").click(function() {
-    var userChosenColor = $(this).attr('id')
-    userClickedPattern.push(userChosenColor)
-    playSound(userChosenColor)
-    animatePress(userChosenColor)
 
-});
+
 
 
 
