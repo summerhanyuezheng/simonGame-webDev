d29ede69416114f74834bc6ae46a9c0d178d3747
summerhanyuezheng <summer199303@gmail.com>
2023-04-19 01:16:26 -0700

    added level display with keypress

--- game.js
+++ game.js
@@ -2,6 +2,18 @@ const gamePattern = []
 const buttonColours = ["red", "blue", "green", "yellow"]
 const userClickedPattern = []
 
+var started = false
+var level = 0
+
+$(document).keypress(function() {
+    if (!started){
+        started = true
+        $("h1").text("Level 0")
+        nextSequence()
+    }
+
+})
+
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3" )
@@ -18,15 +30,17 @@ function animatePress(currentColor){
 
 
 
-
-
 function nextSequence() {
+
+    level++
+    $("h1").text("Level " + level)
     // generate random number from 0-3
     var randomNumber = Math.floor(Math.random() * 4)
     var randomChosenColor = buttonColours[randomNumber]
     gamePattern.push(randomChosenColor)
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor)
+    
 }
 
 
